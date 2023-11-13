92e4ffe696122eb71925f812503e3d9096b13595
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-14 00:54:46 +0600

    Create Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      <Link to="/exam">Start Exam</Link>
+      {/* Add other dashboard components or links as needed */}
+    </div>
+  );
+};
+
+export default Dashboard;
