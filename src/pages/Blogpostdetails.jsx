8f202916cf16e32e9d91c3e8cd17593e09b111cc
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-14 14:04:40 +0600

    Update Blogpostdetails.jsx

--- src/pages/Blogpostdetails.jsx
+++ src/pages/Blogpostdetails.jsx
@@ -22,7 +22,7 @@ const BlogPostDetails = ({ post }) => {
         </ul>
 
         <h1 className="page-heading my-8 mt-10 text-center text-3xl font-bold uppercase text-black dark:text-white md:text-5xl">{post.title}</h1>
-        <div className="mt-10 text-center text-2xl font-bold text-black dark:text-white md:text-2xl">{post.details}</div>
+        <div className="mt-10 text-center text-1xl font-bold text-black dark:text-white md:text-1xl">{post.details}</div>
 
         {/* Your blog post content goes here */}
 
