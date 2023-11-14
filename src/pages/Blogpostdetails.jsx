7a3a230c28def91ccf3481b4762dcc5f74bce0c2
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-14 13:49:29 +0600

    Update Blogpostdetails.jsx

--- src/pages/Blogpostdetails.jsx
+++ src/pages/Blogpostdetails.jsx
@@ -23,7 +23,7 @@ const BlogPostDetails = ({ post }) => {
 
         <h1 className="page-heading my-8 mt-10 text-center text-3xl font-bold uppercase text-black dark:text-white md:text-5xl">{post.title}</h1>
         <div class="post-meta-text">{post.details}</div>
-
+<div class="mt-10 text-center text-1xl font-bold text-black dark:text-white md:text-1xl">In today's digital age, blogging has become a powerful medium for individuals, businesses, and creators to share their thoughts, ideas, and expertise with the world. One of the most popular platforms for creating a blog is Blogger, a user-friendly platform owned by Google. Whether you're a beginner looking to start a personal blog or an aspiring professional blogger aiming to reach a wider audience, this guide will take you through the step-by-step process of creating a blog in Blogger, from the basics to pro-level strategies.</div>
         {/* Your blog post content goes here */}
 
         <a href={post.link} className="btn btn-outline">
