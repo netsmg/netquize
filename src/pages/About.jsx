fa94c02140fb97a9cb6fbbd8a3a5d6c7d8023ccc
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-14 15:29:20 +0600

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>About NetQuize</h2>
-      <p>Welcome to NetQuize, your go-to platform for creating and storing quizzes!</p>
-      <p>With NetQuize, you can:</p>
+    <div className="mx-8 flex flex-col items-center justify-center gap-2">
+        <p className="page-heading my-8 mt-10 text-center text-3xl font-bold uppercase text-black dark:text-white md:text-5xl">
+          The Best
+          <span className="my-1 block text-center text-primary drop-shadow-2xl">
+            Coding Quiz Practice
+          </span>
+          Platform you have ever seen!
+        </p>
+        
+      <p className="rounded-lg shadow-xl px-4 py-2 text-center font-semibold uppercase tracking-wide text-black md:text-xl">Welcome to NetQuize, your go-to platform for creating and storing quizzes!</p>
+      <div className="rounded-lg shadow-xl px-4 py-2 text-center font-semibold uppercase tracking-wide text-black md:text-xl">
+      <p className="rounded-lg shadow-xl px-4 py-2 text-center font-semibold uppercase tracking-wide text-black md:text-xl">With NetQuize, you can:</p>
       <ul>
         <li>Create custom quizzes tailored to your needs.</li>
         <li>Store and organize quizzes for easy access.</li>
@@ -14,7 +22,11 @@ const About = () => {
       </ul>
       <p>NetQuize is designed to make the quiz creation and management process seamless and efficient.</p>
       <p>Get started today and enhance your learning experience with NetQuize!</p>
-    </div>
+    
+      </div>
+    <div>
+      
+       
   );
 };
 
