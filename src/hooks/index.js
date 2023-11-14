434812a534608c3894cea848bf27925c3f5d79c1
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-14 20:07:14 +0600

    Update index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useData } from './useData';
 export { default as usePopularQuizzes } from './usePopularQuizzes';
 export { default as useQuiz } from './useQuiz';
+export { default as useQuize } from './useQuize';
 export { default as useSubmissions } from './useSubmissions';
