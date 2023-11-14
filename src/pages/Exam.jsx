65c27496d5e6b0e00f47f223bbf354d8d8c0a9b8
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-14 09:06:38 +0600

    Update Exam.jsx

--- src/pages/Exam.jsx
+++ src/pages/Exam.jsx
@@ -7,7 +7,23 @@ const Exam = () => {
   const [title, setTitle] = useState('');
   const [noq, setNoq] = useState('');
 
-  
+  const handleFormSubmit = (e) => {
+    
+
+    // Initialize Firebase (replace with your own config)
+    const firebaseConfig = {
+      apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
+  authDomain: "contact-database-9c47b.firebaseapp.com",
+  databaseURL: "https://contact-database-9c47b-default-rtdb.firebaseio.com",
+  projectId: "contact-database-9c47b",
+  storageBucket: "contact-database-9c47b.appspot.com",
+  messagingSenderId: "118822537955",
+  appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
+  measurementId: "G-0WY0798WFC"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
     // Create a reference to the 'quizzes' node in the database
     const database = firebase.database();
     const quizzesRef = database.ref('quizzes');
