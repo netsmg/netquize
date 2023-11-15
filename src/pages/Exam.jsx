7045a00b314f707ec74b170b0a3d0c0616dac583
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-15 15:24:44 +0600

    Update Exam.jsx

--- src/pages/Exam.jsx
+++ src/pages/Exam.jsx
@@ -1,80 +1,193 @@
-import { child, get, getDatabase, push, ref, update } from 'firebase/database';
-import _ from 'lodash';
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
 
-import { PageNotFound } from './';
+          // dependencies
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { nanoid } from 'nanoid';
 
-import { AnswerBox, ProgressBar, Rules } from '../components';
-import { useAuth } from '../contexts/AuthContext';
-import { useQuize } from '../hooks';
 
-function Exam() {
-  const { id } = useParams();
-  const { loading, error, quiz } = useQuize(id);
-  const [qnaSet, dispatch] = useReducer(reducer, initialState);
-  const { currentUser } = useAuth();
-  const navigate = useNavigate();
-  const date = useMemo(() => new Date(), []);
 
+// components
+import MessageBubble from '../components/MessageBubble';
+
+// firebase
+
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  onSnapshot,
+  orderBy,
+  query,
+  Timestamp,
+} from 'firebase/firestore';
+
+export default function Chat({ user }) {
+  const body = document.body;
+  let navigate = useNavigate();
+  const [scrollY, setScrollY] = useState(0);
+  const [showScroll, setShowScroll] = useState(false);
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+  const [showInput, setShowInput] = useState(false);
+  const [isMessageValid, setIsMessageValid] = useState(false);
+
+  // navigate to '/welcome' page if user is not logged in
   useEffect(() => {
-    dispatch({
-      type: 'quiz',
-      value: quiz
-    });
-  }, [quiz]);
+    if (!user) {
+      return navigate('/chat');
+    }
+  }, [user]);
 
-  // Answer option selection
-  const handleAnswerChange = useCallback(
-    (e, questionIndex, optionIndex) => {
-      dispatch({
-        type: 'answer',
-        questionID: questionIndex,
-        optionIndex: optionIndex,
-        value: e.target.checked
+  // checks for scroll event in body
+  //  => if scrolled scrollY state is changed to scroll position
+  useEffect(() => {
+    function watchScroll() {
+      body.addEventListener('scroll', () => {
+        setScrollY(body.scrollTop);
       });
-    },
-    [dispatch]
-  );
+    }
+    watchScroll();
+    return () => {
+      body.removeEventListener('scroll', () => {
+        setScrollY(body.scrollTop);
+      });
+    };
+  });
 
-  // Submit Quiz and store result in the database
-  const submitQuiz = useCallback(async () => {
-    // ... (existing logic)
-  }, [currentUser, date, id, navigate, qnaSet]);
+  // change showScroll state when page is scrolled up
+  useEffect(() => {
+    if (scrollY < body.scrollHeight / 1.65) {
+      setShowScroll(true);
+    } else {
+      setShowScroll(false);
+    }
+  }, [scrollY]);
+
+  // scrolls to bottom of page(body)
+  const scrollToBottom = () => {
+    body.scrollTo(0, body.scrollHeight);
+  };
+
+  // calls scrollToBottom() functino
+  //  => when message is updated
+  //  => when page loads
+  useEffect(() => {
+    scrollToBottom();
+  }, [messageList]);
+
+  // checks & updates messages in real-time
+  //  => sets messageList state to list of messages from firebase db
+  useEffect(() => {
+    const q = query(collection(db, 'messages'), orderBy('postedDate'));
+    onSnapshot(q, (querySnapshot) => {
+      const messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push(doc.data());
+      });
+      setMessageList(messages);
+    });
+  }, []);
+
+  // check if message is valid
+  //  => check if user entered message or not
+  //  => if not option to close input box is visible
+  useEffect(() => {
+    if (message.length > 0) {
+      setIsMessageValid(true);
+    } else {
+      setIsMessageValid(false);
+    }
+  }, [message]);
+
+  // when user submits the form(message input)
+  //  => adds message to firebase db
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const docRef = doc(db, 'users', user.uid);
+    const docSnap = await getDoc(docRef);
+    await addDoc(collection(db, 'messages'), {
+      author: docSnap.data(),
+      body: message,
+      postedDate: Timestamp.now(),
+    });
+    setMessage('');
+  };
 
   return (
     <>
-      {loading && <p className="page-heading text-lg">Loading ...</p>}
-      {error && <PageNotFound />}
-      {!loading && !error && qnaSet && qnaSet?.length === 0 && <PageNotFound />}
-      {!loading && !error && qnaSet && qnaSet?.length > 0 && (
-        <div className="mx-auto w-[85%] animate-reveal">
-          <h1 className="page-heading">{id.split('-').join(' ')} Quiz!</h1>
-          <Rules />
-          <div className="card mb-40 flex flex-col justify-center rounded-md p-3">
-            {qnaSet.map((question, questionIndex) => (
-              <div key={questionIndex} className="mb-8">
-                <div className="flex flex-col items-center justify-center text-xl font-bold text-black dark:text-white sm:text-3xl">
-                  Q. {question.title}
-                </div>
-                <hr className="mb-3 mt-3 h-px border-0 bg-primary" />
-                <AnswerBox
-                  input
-                  handleChange={(e, optionIndex) =>
-                    handleAnswerChange(e, questionIndex, optionIndex)
-                  }
-                  options={question.options}
-                />
-              </div>
-            ))}
-          </div>
-          <button onClick={submitQuiz} className="btn btn-primary">
-            Submit Quiz
+      <div className='chat'>
+        {messageList &&
+          messageList.map((message) => (
+            <MessageBubble message={message} user={user} key={nanoid()} />
+          ))}
+
+        {!showInput && showScroll && (
+          <button onClick={scrollToBottom} className='btn scroll-top'>
+            <IoIosArrowDown />
           </button>
-        </div>
-      )}
+        )}
+
+        <form
+          className={`message-form ${showInput ? 'show-input' : ''}`}
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          {showInput && (
+            <>
+              <div className='input-container'>
+                <textarea
+                  type='text'
+                  className='message-form__input'
+                  name='message'
+                  placeholder='Message'
+                  value={message}
+                  autoComplete='off'
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+                {/* /input */}
+
+                {isMessageValid ? (
+                  <button
+                    type='submit'
+                    className='message-form__input-btn submit'
+                  >
+                    <FaRegPaperPlane />
+                  </button>
+                ) : (
+                  <button
+                    type='button'
+                    className='message-form__input-btn'
+                    onClick={() => {
+                      setShowInput(false);
+                    }}
+                  >
+                    <RxCross2 />
+                  </button>
+                )}
+                {/* /hide-input-btn */}
+              </div>
+              {/* /input-container */}
+            </>
+          )}
+
+          {!showInput && (
+            <button
+              type='button'
+              className='btn message-form__btn'
+              onClick={() => {
+                setShowInput(true);
+              }}
+            >
+              <FaRegPaperPlane />
+            </button>
+          )}
+        </form>
+        {/* /message-form */}
+      </div>
     </>
   );
 }
+
+
 
 export default Exam;
