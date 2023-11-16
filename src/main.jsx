67841a41ccb47499d8d4e7ede978d176bfc7e1fd
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-16 13:17:52 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import * as serviceWorker from "./swDev";
+
 
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(<App />);
 
-// Register the service worker
-serviceWorker.register();
+
