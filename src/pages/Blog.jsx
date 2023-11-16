41477bd3fdc71fb9b2fa19388886dc277189fd83
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-17 00:54:35 +0600

    Update Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -16,6 +16,31 @@ const Blog = () => {
       readingTime: 3,
       image: "https://codewithsadee.github.io/vast/assets/images/blog-1.jpg",
     },
+   {
+      id: 2,
+      title: "Lenovo’s smarter devices stoke professional passions",
+      categories: ["Garden", "Videos"],
+      author: {
+        name: "Lila",
+        avatar: "https://codewithsadee.github.io/vast/assets/images/blog-1.jpg",
+      },
+      date: "Sep 18, 2021",
+      readingTime: 3,
+      image: "https://codewithsadee.github.io/vast/assets/images/blog-1.jpg",
+    },
+    {
+      id: 3,
+      title: "Lenovo’s smarter devices stoke professional passions",
+      categories: ["Garden", "Videos"],
+      author: {
+        name: "Lila",
+        avatar: "https://codewithsadee.github.io/vast/assets/images/blog-1.jpg",
+      },
+      date: "Sep 18, 2021",
+      readingTime: 3,
+      image: "https://codewithsadee.github.io/vast/assets/images/blog-1.jpg",
+    },
+   
    
   ];
 return ( <div className="container mx-auto mx-auto mb-32 flex min-h-screen w-[90%] animate-reveal flex-col items-center wp-block-group container is-layout-flow wp-block-group-is-layout-flow"> {blogData.map((blog) => ( <Link key={blog.id} to={`https://netquize.vercel.app/blog/${blog.id}`} className="text-black dark:text-white"> <Blogpost {...blog} /> </Link> ))} </div>
