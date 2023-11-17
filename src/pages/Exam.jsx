21388376d355f5d4103d5e37b1301322bc1b7c5f
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-17 13:02:57 +0600

    Update Exam.jsx

--- src/pages/Exam.jsx
+++ src/pages/Exam.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref as dbRef, push } from 'firebase/database';
 
-const Exam = () => {
-  return (
-    <div className="flex items-center justify-center h-screen bg-blue-500">
-      <div className="text-white text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to NetSmg</h1>
-        <p className="text-lg">Start your journey here!</p>
-      </div>
-    </div>
-  );
+export default {
+  setup() {
+    const video = ref({
+      title: '',
+      link: '',
+    });
+
+    const submitForm = () => {
+      saveToFirebase(video.value);
+    };
+
+    const saveToFirebase = (videoData) => {
+      // Replace this with your Firebase config
+      const firebaseConfig = {
+        // Your Firebase config here
+      };
+
+      const firebaseApp = initializeApp(firebaseConfig);
+      const database = getDatabase(firebaseApp);
+      const videosRef = dbRef(database, 'videos');
+
+      // Push the video data to the "videos" node
+      push(videosRef, videoData);
+
+      // Clear the form after submission
+      video.value = { title: '', link: '' };
+
+      // Optional: Log success message or perform other actions
+      console.log('Data saved to Firebase!');
+    };
+
+    return {
+      video,
+      submitForm,
+    };
+  },
 };
-
-export default Exam;
