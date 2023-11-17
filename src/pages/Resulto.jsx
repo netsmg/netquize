649b89e432d4b6fa7025d0b5f3f31adb7cad76b8
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-17 22:13:24 +0600

    Update Resulto.jsx

--- src/pages/Resulto.jsx
+++ src/pages/Resulto.jsx
@@ -1,37 +1,116 @@
 import React from 'react';
+export default function Resulto() {
+  const { isAuthenticated, isReady } = useAuth();
+  const { login, loading, error, data } = useLogin();
+  const router = useRouter();
 
-const Resulto = ({ selectedAnswers }) => {
-  // Implement your result calculation logic based on selected answers
-  // For simplicity, assuming correct answers are known in advance
-  const correctAnswers = {
-    // Map question IDs to correct option IDs
-    // Replace these with the actual correct answers from your data
-    1: 'A',
-    2: 'C',
-    3: 'D',
-    4:	'E',
-    // Add more as needed
-  };
+  if (isReady && isAuthenticated) {
+    router.replace("/");
+  }
 
-  const calculateScore = () => {
-    let score = 0;
-    for (const questionId in selectedAnswers) {
-      if (selectedAnswers[questionId] === correctAnswers[questionId]) {
-        score++;
-      }
+  const errorMessage = error?.message || data?.generateAuthorizationCode.error;
+
+  const handleClickLostPassword = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (
+      NC_SITE_SETTINGS.signIn_page.lost_password
+        .is_use_lostpassword_default_of_wp === false
+    ) {
+      e.preventDefault();
+      toast.error(NC_SITE_SETTINGS.signIn_page.lost_password.message, {
+        position: "bottom-center",
+      });
+      return;
     }
-    return score;
   };
-
-  const score = calculateScore();
 
   return (
-    <div className="result-container">
-      <h1>Result</h1>
-      <p>Your Score: {score} out of {Object.keys(selectedAnswers).length}</p>
-      {/* Display more result details or feedback as needed */}
-    </div>
-  );
-};
+    <LoginLayout
+      isLoginPage
+      rightBtn={{
+        text: "Sign up",
+        href: "/sign-up",
+      }}
+    >
+      <>
+        <div className="grid gap-6">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
 
-export default Resulto;
+              if (
+                !e.currentTarget.username?.value ||
+                !e.currentTarget.password?.value
+              ) {
+                toast.error("Username and password are required!", {
+                  position: "bottom-center",
+                });
+                return;
+              }
+
+              login(
+                e.currentTarget.username.value,
+                e.currentTarget.password.value,
+                "/"
+              );
+            }}
+          >
+            <div className="grid gap-4">
+              <div className="grid gap-1.5">
+                <Label htmlFor="email">Username</Label>
+                <Input
+                  id="username"
+                  name="username"
+                  placeholder="Email or username"
+                  autoCapitalize="none"
+                  autoComplete="username"
+                  autoCorrect="off"
+                  type="text"
+                  required
+                  defaultValue={IS_CHISNGHIAX_DEMO_SITE ? "demo" : undefined}
+                />
+              </div>
+              <div className="grid gap-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  defaultValue={IS_CHISNGHIAX_DEMO_SITE ? "demo" : undefined}
+                />
+              </div>
+              <div className="grid">
+                <ButtonPrimary loading={loading}>Sign in</ButtonPrimary>
+                {!!errorMessage && (
+                  <Error className="text-center mt-2" error={errorMessage} />
+                )}
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <p className="text-center text-sm leading-6 text-neutral-500 dark:text-neutral-400">
+          Not a member?{" "}
+          <Link
+            href="/sign-up"
+            className="font-medium text-primary-600 hover:text-primary-500 dark:text-primary-400 hover:underline underline-offset-2"
+          >
+            Sign up!
+          </Link>
+          <span className="mx-1">|</span>
+          <a
+            href={
+              NC_SITE_SETTINGS.signIn_page.lost_password
+                .url_lostpassword_default_of_wp
+            }
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium text-primary-600 hover:text-primary-500 dark:text-primary-400 hover:underline underline-offset-2"
+            onClick={handleClickLostPassword}
+          >
+            Lost your password?
+          </a>
+        </p>
+      </>
+    </LoginLayout>
+  );
+                }
