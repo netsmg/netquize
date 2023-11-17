edf41ac40341863adfd67221dc0a9b8184f4df36
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-17 17:34:31 +0600

    Update firebase.js

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -16,13 +16,14 @@ try {
   app = getApp();
 } catch (error) {
   const firebaseConfig = {
-    apiKey: "AIzaSyBzKuzjKriUidssayDTBTnOzhYhI6EgmbY",
-    authDomain: "web-chat-app-2-f13ae.firebaseapp.com",
-    projectId: "web-chat-app-2-f13ae",
-    storageBucket: "web-chat-app-2-f13ae.appspot.com",
-    messagingSenderId: "229937947974",
-    appId: "1:229937947974:web:0ed237852f9333d6977147"
-  };
+    apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
+  authDomain: "contact-database-9c47b.firebaseapp.com",
+  databaseURL: "https://contact-database-9c47b-default-rtdb.firebaseio.com",
+  projectId: "contact-database-9c47b",
+  storageBucket: "contact-database-9c47b.appspot.com",
+  messagingSenderId: "118822537955",
+  appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
+  measurementId: "G-0WY0798WFC" };
   app = initializeApp(firebaseConfig);
 }
 storage = getStorage(app);
