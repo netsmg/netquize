d7d93a49cf0671f20b58cd888725edd05c781188
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-19 12:22:35 +0600

    Update Exam.jsx

--- src/pages/Exam.jsx
+++ src/pages/Exam.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Exam = () => {
   return (
     <div className="iframe">
-    <iframe src="https://standardmindgame.vercel.app/app/views/exam.html" width="100%" height="768px"/>
+    <iframe src="https://standardmindgame.vercel.app/app/views/exam.html" width="100%" height="100%"/>
       </div>
   );
 };
