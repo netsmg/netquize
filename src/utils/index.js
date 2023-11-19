3a37167bf5e5d2262bff3d36640cb3ddb65d563a
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-19 18:16:31 +0600

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,3 @@
-export { default as  } from './useAuth';
-export { default as useData } from './useData';
-export { default as usePopularQuizzes } from './usePopularQuizzes';
-export { default as useQuiz } from './useQuiz';
-export { default as useQuize } from './useQuize';
-export { default as useSubmissions } from './useSubmissions';
+export { default as CorrectAnswerRadioButtons } from './correct_answer';
+export { default as QuizCategorySelect } from './category_quiz';
+export { default as Private } from './private';
