7547f083725c985fd6cdc2bc3e18d6588f20f6ee
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-19 12:56:16 +0600

    Update Notification.jsx

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,48 +1,10 @@
-import { useEffect } from 'react';
-
-import 'firebase/messaging';
+import React from 'react';
 
 const Notification = () => {
-  useEffect(() => {
-    const initializeFirebaseMessaging = async () => {
-      try {
-        const firebaseConfig = {
-          apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
-  authDomain: "contact-database-9c47b.firebaseapp.com",
-  databaseURL: "https://contact-database-9c47b-default-rtdb.firebaseio.com",
-  projectId: "contact-database-9c47b",
-  storageBucket: "contact-database-9c47b.appspot.com",
-  messagingSenderId: "118822537955",
-  appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
-  measurementId: "G-0WY0798WFC"
-          
-        };
-
-        firebase.initializeApp(firebaseConfig);
-
-        const messaging = firebase.messaging();
-
-        await messaging.requestPermission();
-        const token = await messaging.getToken();
-        console.log('FCM Token:', token);
-
-        messaging.onMessage((payload) => {
-          console.log('Message received:', payload);
-          // Handle the notification payload and display it to the user
-        });
-      } catch (error) {
-        console.error('Error initializing Firebase Messaging:', error);
-      }
-    };
-
-    initializeFirebaseMessaging();
-  }, []);
-
   return (
-    <div>
-      {/* You can customize the UI for notifications here */}
-      {/* For example, display a notification message when a new message is received */}
-    </div>
+    <div className="iframe">
+    <iframe src="https://quizzy-ruby.vercel.app" width="100%" height="1080px"/>
+      </div>
   );
 };
 
