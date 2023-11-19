f03ade8b023e7c339c683d352a1b5be044384c18
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-19 18:12:27 +0600

    Create index.js

---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+export { default as  } from './useAuth';
+export { default as useData } from './useData';
+export { default as usePopularQuizzes } from './usePopularQuizzes';
+export { default as useQuiz } from './useQuiz';
+export { default as useQuize } from './useQuize';
+export { default as useSubmissions } from './useSubmissions';
