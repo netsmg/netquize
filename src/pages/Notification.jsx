7a5edfcae26085e0cda8066b03999785cd49b8de
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-19 13:53:11 +0600

    Update Notification.jsx

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Notification = () => {
   return (
     <div className="iframe">
-    <iframe src="https://quizzy-ruby.vercel.app" width="100%" height="1080px"/>
+    <iframe src="https://wp-coders.vercel.app" width="100%" height="1080px"/>
       </div>
   );
 };
