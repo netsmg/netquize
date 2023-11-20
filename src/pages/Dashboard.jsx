9e62b6ade519eacacf745251c1da8d61b0d9d260
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-20 17:05:35 +0600

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,181 +1,122 @@
-import React, { Fragment, useEffect, useRef, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import {
-  Box,
-  Card,
-  Typography,
-  CardContent,
-  makeStyles,
-  FormControl,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-  Button,
-  styled,
-} from "@material-ui/core";
-import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
-import IconButton from "@material-ui/core/IconButton";
-import { QuizCategorySelect } from "../../utils/db";
-import Form from "../../components/Form/Form";
+import React, { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 
-const useStyles = makeStyles({
-  root: {
-    marginTop: "20px",
-  },
-  boxCorrect: {
-    backgroundColor: "#91c788",
-    color: "white",
-    padding: ".7rem",
-  },
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
+  authDomain: "contact-database-9c47b.firebaseapp.com",
+  databaseURL: "https://contact-database-9c47b-default-rtdb.firebaseio.com",
+  projectId: "contact-database-9c47b",
+  storageBucket: "contact-database-9c47b.appspot.com",
+  messagingSenderId: "118822537955",
+  appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
+}
 
-const LinkButton = styled(Link)({
-  width: "100%",
-  textAlign: "center",
-  marginTop: "20px",
-  textDecoration: "none",
-  backgroundColor: "#81c784",
-  color: "white",
-  padding: ".5rem",
-});
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-function Dashboard() {
-  const classes = useStyles();
-  const params = useParams();
-  const refGrade = useRef();
-
-  const [quiz, setQuiz] = useState([]);
-  const [questionAnswered, setQuestionAnswered] = useState([]);
-  const [lengthQuestion, setLengthQuestion] = useState(0);
-  const [disableButton, setDisableButton] = useState(false);
-  const [btnSubmitHasClick, setBtnSubmitHasClick] = useState(false);
-  const [open, setOpen] = useState(false);
-  const [boxCorrectAnswer, setBoxCorrectAnswer] = useState({});
+const Dashboard = () => {
+  const [quizzes, setQuizzes] = useState([]);
+  const [currentQuiz, setCurrentQuiz] = useState(null);
+  const [userAnswers, setUserAnswers] = useState({});
 
   useEffect(() => {
-    getQuizByCategory(params.category).then((quiz) => {
-      setQuiz(quiz);
-      setLengthQuestion(quiz.length);
+    const fetchQuizzes = async () => {
+      const quizzesRef = collection(db, 'quizzes');
+      const quizzesSnapshot = await getDocs(quizzesRef);
+
+      const quizData = quizzesSnapshot.docs.map((doc) => {
+        const quiz = doc.data();
+        quiz.id = doc.id;
+        return quiz;
+      });
+
+      setQuizzes(quizData);
+    };
+
+    fetchQuizzes();
+  }, []);
+
+  const handleStartQuiz = (quiz) => {
+    setCurrentQuiz(quiz);
+    setUserAnswers({});
+  };
+
+  const handleAnswerSelection = (questionId, selectedOption) => {
+    setUserAnswers({
+      ...userAnswers,
+      [questionId]: selectedOption,
     });
+  };
 
-    return () => setQuiz([]);
-  }, [params.category, open]);
-
-  const handleChangeRadio = (e) => {
-    const index = e.target.name;
-    const value = e.target.value;
-
-    questionAnswered.splice(index, index + 1, value);
-    setQuestionAnswered([...questionAnswered]);
-
-    questionAnswered.forEach((checked, index) => {
-      if (checked !== quiz[index].correctAnswer) {
-        setBoxCorrectAnswer({
-          ...boxCorrectAnswer,
-          [index]: false,
-        });
+  const handleSubmitQuiz = async () => {
+    const correctAnswers = currentQuiz.questions.map((question) => question.correctAnswer);
+    const userScore = userAnswers.questions.reduce((score, answer, index) => {
+      if (answer === correctAnswers[index]) {
+        score++;
       }
-    });
-  };
+      return score;
+    }, 0);
 
-  const checkQuestionThatAnswered = () => {
-    const correct = quiz.map((question) => {
-      return question.correctAnswer;
-    });
+    const quizSubmission = {
+      quizId: currentQuiz.id,
+      userAnswers: userAnswers,
+      score: userScore,
+    };
 
-    const checkAnsweredQuestion = questionAnswered.filter((answered, i) => {
-      return answered === correct[i];
-    }).length;
+    await addDoc(collection(db, 'quizSubmissions'), quizSubmission);
 
-    setBtnSubmitHasClick(true);
-    setDisableButton(true);
-
-    refGrade.current.textContent = `Score: ${gradeOfQuiz(checkAnsweredQuestion)}`;
-
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  };
-
-  const gradeOfQuiz = (correct) => {
-    return Math.round((correct / lengthQuestion) * 100);
-  };
-
-  const showdDialogFormPostQuiz = () => {
-    setOpen(true);
+    alert(`Your score: ${userScore}`);
+    setCurrentQuiz(null);
   };
 
   return (
-    <Fragment>
-      <Box display="flex" justifyContent="space-between">
-        <Typography variant="h5">
-          {`${params.category ?? ""}`.toUpperCase()}
-        </Typography>
-        <Typography variant="h5" ref={refGrade}>
-          Score:{" "}
-        </Typography>
-      </Box>
-      <Box>
-        {quiz.map((quiz, id) => (
-          <Card key={id} className={classes.root}>
-            <CardContent>
-              <Typography component="p">{quiz.question}</Typography>
-              <FormControl disabled={disableButton}>
-                <RadioGroup name={`${id}`} onChange={handleChangeRadio}>
-                  <MultipleChoice answer={quiz.answer} />
-                </RadioGroup>
-              </FormControl>
-            </CardContent>
-            <div className={classes.boxCorrect}>
-              {boxCorrectAnswer[id] === false && btnSubmitHasClick ? (
-                <Typography>{quiz.answer[quiz.correctAnswer]}</Typography>
-              ) : (
-                <></>
-              )}
-            </div>
-          </Card>
-        ))}
-        {lengthQuestion > 0 ? (
-          <Box display="flex" flexDirection="column" marginTop={4} marginBottom={4}>
-            <Button disabled={disableButton} variant="contained" color="primary" onClick={checkQuestionThatAnswered}>
-              Submit
-            </Button>
-            {disableButton ? <LinkButton to="/">Back to Home</LinkButton> : <></>}
-          </Box>
-        ) : (
-          <></>
-        )}
-      </Box>
-      <Box position="fixed" bottom={20} right={20}>
-        <IconButton onClick={showdDialogFormPostQuiz}>
-          <AddCircleOutlineIcon />
-        </IconButton>
-      </Box>
-      <Form open={open} setOpen={setOpen} type="form-question" />
-    </Fragment>
+    <div>
+      <h1>Quiz App</h1>
+
+      {quizzes.length > 0 ? (
+        <ul>
+          {quizzes.map((quiz) => (
+            <li key={quiz.id}>
+              <button onClick={() => handleStartQuiz(quiz)}>
+                {quiz.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No quizzes available</p>
+      )}
+
+      {currentQuiz && (
+        <div>
+          <h2>{currentQuiz.title}</h2>
+          <ul>
+            {currentQuiz.questions.map((question, index) => (
+              <li key={question.id}>
+                <h3>{question.text}</h3>
+                <ul>
+                  {question.options.map((option) => (
+                    <li key={option.id}>
+                      <input
+                        type="radio"
+                        name={question.id}
+                        value={option.text}
+                        checked={userAnswers[question.id] === option.text}
+                        onChange={() => handleAnswerSelection(question.id, option.text)}
+                      />
+                      {option.text}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+          <button onClick={handleSubmitQuiz}>Submit Quiz</button>
+        </div>
+      )}
+    </div>
   );
-}
-
-function MultipleChoice({ answer }) {
-  const classes = useStyles();
-  const result = [];
-
-  for (const [mc, theAnswer] of Object.entries(answer)) {
-    result.push({
-      choice: mc,
-      answer: theAnswer,
-    });
-  }
-
-  return (
-    <>
-      {result?.map((radio, id) => (
-        <FormControlLabel className={classes.root} key={id} label={radio.answer} value={radio.choice} control={<Radio />} />
-      ))}
-    </>
-  );
-}
+};
 
 export default Dashboard;
