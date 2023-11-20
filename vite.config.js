513bd681f098621a01966a42e8d7fa275261d790
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-20 19:22:30 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,28 @@
+import react from '@vitejs/plugin-react';
+import VitePWA from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
-import React from 'react';
-import { VitePWAOptions } from 'vite-plugin-pwa';
 
-
-
-const pwaPlugin: VitePWAOptions = {
-  manifest: {
-    
-    name: 'NetQuize',
-    short_name: 'NetQuize',
-    description: 'Your description here',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    icons: [
-      
-      {
-        src: '/path/to/icon.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/path/to/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
-  },
-};
 
 export default defineConfig({
-  // ... other Vite config options
   plugins: [
-    reactPlugin(),
-    VitePWA(pwaPlugin), 
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'NetQuize',
+        short_name: 'NetQuize',
+        description: 'NetQuize',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+      },
+      workbox: {
+        
+      },
+    }),
   ],
+  server: {
+    host: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+  },
 });
-
-function reactPlugin() {
-  return `
-    import React from 'react';
-    import ReactDOM from 'react-dom';
-    import App from './App'; 
-
-    ReactDOM.render(
-      React.createElement(React.StrictMode, null, React.createElement(App, null)),
-      document.getElementById('root')
-    );
-  `;
-}
