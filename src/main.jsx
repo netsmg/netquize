b6b9a2267690e3c2a9241b762b5b8549c03a0d39
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-21 15:17:26 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import "./assets/css/style.css";
+import "./assets/css/result.css";
+import "./assets/css/home.css";
+import "./assets/css/quiz.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "./context/QuizContext";
 
 
 import App from './App';
