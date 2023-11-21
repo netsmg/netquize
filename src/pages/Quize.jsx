7f9d47e50b7c4e2fd65f93951e3f5d01d5a72ddf
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-21 16:37:48 +0600

    Update Quize.jsx

--- src/pages/Quize.jsx
+++ src/pages/Quize.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useQuizContext } from "../context/QuizContext";
+
 import { useNavigate, useParams } from "react-router-dom";
 
 function Quize() {
