d4340600c0b713c134b38c41e286af76df2f2c09
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-25 14:00:28 +0600

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import { SignInWithGoogleButton, SignUpForm } from '../components';
 
 function SignUp() {
   return (
-    <div className="bg-gray-50 dark:bg-gray-900">
+    <div className="bg-gray-50 dark:bg-gray-900 space-y-4 md:space-y-6">
       <h1 className="page-heading">Create an account</h1>
 
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
