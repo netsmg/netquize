a5bd6a867775c12004145f8dd3e9a92f09fb8317
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-25 13:53:37 +0600

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,10 +2,10 @@ import { SignInWithGoogleButton, SignUpForm } from '../components';
 
 function SignUp() {
   return (
-    <div className="mx-auto flex min-h-screen w-[85%] animate-reveal flex-col items-center pb-10">
+    <div className="bg-gray-50 dark:bg-gray-900">
       <h1 className="page-heading">Create an account</h1>
 
-      <div className="card w-full max-w-xl">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <SignUpForm />
       </div>
       <div className="mx-auto w-full max-w-xl">
