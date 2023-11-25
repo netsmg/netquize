cfc5f658c619b1362badaab60ea8ed6ef0a99bcb
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-25 13:11:47 +0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,12 @@ import { LoginForm, SignInWithGoogleButton } from '../components';
 
 function Login() {
   return (
-    <div className=" w-[85%] animate-reveal items-center">
+    <div className="animate-reveal items-center">
       <h1 className="page-heading">Log in to Quizzy</h1>
-      <div className="card mx-auto w-full max-w-xl">
+      <div className="w-full">
         <LoginForm />
       </div>
-      <div className="mx-auto w-full max-w-xl">
+      <div className="w-full max-w-xl">
         <SignInWithGoogleButton title="Log In" />
       </div>
     </div>
