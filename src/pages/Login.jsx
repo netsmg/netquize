e5c7089d1f8ca184e98452ba5b41a52f0f3c6ac9
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-25 12:56:14 +0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,9 @@ import { LoginForm, SignInWithGoogleButton } from '../components';
 function Login() {
   return (
     
-      <div className="card mx-auto w-full max-w-xl">
+      
         <LoginForm />
-      </div>
+    
       <div className="mx-auto w-full max-w-xl">
         <SignInWithGoogleButton title="Log In" />
       </div>
