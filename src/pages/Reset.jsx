bd78ec03adcfd0c8dd548e1cd193189f61562b01
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-25 14:32:20 +0600

    Update Reset.jsx

--- src/pages/Reset.jsx
+++ src/pages/Reset.jsx
@@ -2,7 +2,7 @@ import { ResetPasswordForm } from '../components';
 
 function Reset() {
   return (
-    <div className="mx-auto flex h-screen w-[85%] animate-reveal flex-col items-center">
+    <div className="mx-auto flex h-screen w-[85%] animate-reveal flex-col items-center border-blue-400">
       <h1 className="page-heading">Reset Password</h1>
       <div className="card mx-4 w-full max-w-xl">
         <ResetPasswordForm />
