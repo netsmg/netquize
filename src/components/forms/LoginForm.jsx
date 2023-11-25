ce578af9cfd59c05ffe319b646d639bb34bbdec6
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-25 14:08:50 +0600

    Update LoginForm.jsx

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -48,7 +48,7 @@ function LoginForm() {
       <TextInput
         required
         icon="mail"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         placeholder="Enter Registered Email ID"
         type="email"
         value={email}
