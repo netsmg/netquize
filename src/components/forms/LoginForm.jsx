ef6fdb53a5ee766230888eeefe9f092913482311
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-25 12:29:16 +0600

    Update LoginForm.jsx

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -33,15 +33,19 @@ function LoginForm() {
   }
 
   return (
-  <section class="bg-gray-50 dark:bg-gray-900">
+  
+    <Form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+        <section class="bg-gray-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img class="w-8 h-8 mr-2" src="https://firebasestorage.googleapis.com/v0/b/contact-database-9c47b.appspot.com/o/Logo-270c8115.svg?alt=media&token=e18ab0e7-0abc-4f39-bb96-26f793edea2a" alt="logo"/>
+          NetQuize   
+      </a>
        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Login to your account
               </h1>
-    <Form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
       <TextInput
         required
         icon="mail"
@@ -93,10 +97,11 @@ function LoginForm() {
           instead.
         </span>
       </div>
-    </Form>
-    </div>
+      </div>
     </div>
     </section>
+    </Form>
+    
   );
 }
 
