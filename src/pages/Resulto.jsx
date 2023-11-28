c1d5a4b81fa1519f2b47dce5d343748c86e4a93b
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-28 10:42:54 +0600

    Update Resulto.jsx

--- src/pages/Resulto.jsx
+++ src/pages/Resulto.jsx
@@ -1,37 +1,25 @@
-import React from 'react';
 
-const Resulto = ({ selectedAnswers }) => {
-  // Implement your result calculation logic based on selected answers
-  // For simplicity, assuming correct answers are known in advance
-  const correctAnswers = {
-    // Map question IDs to correct option IDs
-    // Replace these with the actual correct answers from your data
-    1: 'A',
-    2: 'C',
-    3: 'D',
-    4:	'E',
-    // Add more as needed
-  };
+import { Link } from 'react-router-dom';
 
-  const calculateScore = () => {
-    let score = 0;
-    for (const questionId in selectedAnswers) {
-      if (selectedAnswers[questionId] === correctAnswers[questionId]) {
-        score++;
-      }
-    }
-    return score;
-  };
-
-  const score = calculateScore();
+function MyComponent() {
+  const data = { name: 'John', age: 30 };
 
   return (
-    <div className="result-container">
-      <h1>Result</h1>
-      <p>Your Score: {score} out of {Object.keys(selectedAnswers).length}</p>
-      {/* Display more result details or feedback as needed */}
+    <Link to={`/resulto?name=${data.name}&age=${data.age}`}>Go to My Page</Link>
+  );
+}
+
+// Receiving page
+function Resulto(props) {
+  const searchParams = new URLSearchParams(props.location.search);
+  const name = searchParams.get('name');
+  const age = searchParams.get('age');
+
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
     </div>
   );
-};
-
-export default Resulto;
+}
+export default resulto;
