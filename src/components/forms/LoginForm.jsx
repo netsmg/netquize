4dcd8c9cec6703995e5cbc3174879b86041e5f6e
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-28 13:11:10 +0600

    Update LoginForm.jsx

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { Form, TextInput } from '../';
@@ -29,29 +29,36 @@ function LoginForm() {
       setLoading(false);
       showAlert('error', err.code);
     }
-    return null;
   }
 
   return (
-     <Form  onSubmit={handleSubmit}>
-      
-      <div class="mb-4">
-<TextInput required
-        icon="mail"
-        placeholder="Enter Registered Email ID"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)} class="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"  aria-label="Email" aria-describedby="email-addon" />
-</div>
-<div class="mb-4">
-<input type="password" class="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
- placeholder="Enter Password"
+    <Form onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <TextInput
+          required
+          icon="mail"
+          placeholder="Enter Registered Email ID"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
+          aria-label="Email"
+          aria-describedby="email-addon"
+        />
+      </div>
+      <div className="mb-4">
+        <input
+          type="password"
+          className="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow"
+          placeholder="Enter Password"
           type={show ? 'text' : 'password'}
           value={password}
-          onChange={(e) => setPassword(e.target.value)}  aria-label="Password" aria-describedby="password-addon" />
-</div>
-
-      
+          onChange={(e) => setPassword(e.target.value)}
+          aria-label="Password"
+          aria-describedby="password-addon"
+        />
+      </div>
+      <div>
         <button
           className="material-symbols-outlined mx-1 flex cursor-pointer items-center justify-center text-black dark:text-white md:text-3xl"
           type="button"
@@ -62,7 +69,6 @@ function LoginForm() {
           {show ? 'visibility_off' : 'visibility'}
         </button>
       </div>
-
       <div className="ml-auto">
         <Link
           className="link-text justify-self-end text-base font-medium tracking-wide"
@@ -71,11 +77,13 @@ function LoginForm() {
           Forgot Password?
         </Link>
       </div>
-
-      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" disabled={loading} type="submit">
+      <button
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        disabled={loading}
+        type="submit"
+      >
         Log In
       </button>
-
       <div className="text-center text-sm font-medium tracking-wide text-black dark:text-slate-300 md:text-base">
         Don&apos;t have an account?{' '}
         <span className="inline-block">
@@ -86,8 +94,7 @@ function LoginForm() {
         </span>
       </div>
     </Form>
- );
+  );
 }
 
 export default LoginForm;
-
