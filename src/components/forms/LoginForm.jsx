ae45d92086fc0fc03f882b3f7d4b86d1bc0cce4e
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-28 20:57:25 +0600

    Update LoginForm.jsx

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -77,8 +77,7 @@ function LoginForm() {
           Forgot Password?
         </Link>
       </div>
-      <button
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      <button className="fill-button mt-2" 
         disabled={loading}
         type="submit"
       >
