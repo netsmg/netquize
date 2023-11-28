f9ec33fc5a009161bde9e8d867b20b7eb940da3b
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-11-28 10:54:34 +0600

    Update Resulto.jsx

--- src/pages/Resulto.jsx
+++ src/pages/Resulto.jsx
@@ -1,25 +1,33 @@
+import React from 'react';
 
-import { Link } from 'react-router-dom';
-
-function MyComponent() {
-  const data = { name: 'John', age: 30 };
-
+const Resulto = () => {
   return (
-    <Link to={`/resulto?name=${data.name}&age=${data.age}`}>Go to My Page</Link>
-  );
-}
-
-// Receiving page
-function Resulto(props) {
-  const searchParams = new URLSearchParams(props.location.search);
-  const name = searchParams.get('name');
-  const age = searchParams.get('age');
-
-  return (
-    <div>
-      <p>Name: {name}</p>
-      <p>Age: {age}</p>
+    <div className="rounded-lg mx-8 border border-blue items-center justify-center gap-4">
+        <p className="page-heading my-8 mt-10 text-center text-3xl font-bold uppercase text-black dark:text-white md:text-5xl">
+          The Best
+          <span className="my-1 block text-center text-primary drop-shadow-2xl">
+            Coding Quiz Practice
+          </span>
+          Platform you have ever seen!
+        </p>
+        
+      <p className="rounded-lg shadow-xl px-4 py-2 text-center font-semibold tracking-wide text-black md:text-xl">Welcome to NetQuize, your go-to platform for creating and storing quizzes!</p>
+      <div className="rounded-lg shadow-xl px-4 py-2 text-center font-semibold tracking-wide text-black">
+      <p className="rounded-lg shadow-xl px-4 py-2 text-center font-semibold tracking-wide text-black">With NetQuize, you can:</p>
+      <ul>
+        <li>Create custom quizzes tailored to your needs.</li>
+        <li>Store and organize quizzes for easy access.</li>
+        <li>Engage users with interactive and educational content.</li>
+        <li>Track quiz performance and analyze results.</li>
+      </ul>
+      <p>NetQuize is designed to make the quiz creation and management process seamless and efficient.</p>
+      <p>Get started today and enhance your learning experience with NetQuize!</p>
+    
+      </div>
     </div>
+      
+       
   );
-}
-export default resulto;
+};
+
+export default Resulto;
