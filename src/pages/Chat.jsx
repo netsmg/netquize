1a28b65288b7c8819d0713503d36a23013ff924b
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-05 22:19:21 +0600

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-const Chat = () => {
+const Chat = ({ quizId }) => {
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    const leaderboardRef = firebase.database().ref(`leaderboards/${quizId}`);
+
+    // Listen for real-time updates
+    leaderboardRef.on('value', (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        // Convert data to an array or process as needed
+        const leaderboardData = Object.entries(data).map(([userId, score]) => ({
+          userId,
+          score,
+        }));
+        setLeaderboard(leaderboardData);
+      }
+    });
+
+    return () => {
+      // Clean up the listener when component unmounts
+      leaderboardRef.off('value');
+    };
+  }, [quizId]);
+
   return (
-    <div className="iframe">
-    <iframe src="https://standardmindgame.vercel.app/chat.html" width="100%" height="768px"/>
-      </div>
+    <div>
+      <h2>Leaderboard</h2>
+      <ul>
+        {leaderboard.map((entry) => (
+          <li key={entry.userId}>
+            {entry.userId}: {entry.score}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
