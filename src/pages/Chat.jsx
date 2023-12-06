04830de84fed97c6539f45a434696a3b554dc6cf
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-06 18:18:10 +0600

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import {ChatBox } from '../components';
 
 
-import { auth } from "./firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+
 
 function Chat() {
-  const [user] = useAuthState(auth);
+  
 
   return (
     <div className="Chat">
