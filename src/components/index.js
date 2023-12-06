fe019e1438318e863fa37662c858adec84091ca8
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-06 17:41:33 +0600

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -12,6 +12,10 @@ export { default as AnswerBox } from './AnswerBox';
 export { default as AuthenticationComponent } from './AuthenticationComponent';
 export { default as BasicInfo } from './BasicInfo';
 export { default as ContactUs } from './ContactUs';
+
+export { default as Chatbox } from './Chatbox';
+export { default as Message } from './Message';
+export { default as SendMessage } from './SendMessage';
 export { default as CountUpAnimation } from './CountUpAnimation';
 export { default as DesignComponent } from './DesignComponent';
 export { default as DetailedReport } from './DetailedReport';
