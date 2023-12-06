5878b35dfdb2e8f3e0c2b5d0239c0027c5b4d5b0
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-06 10:59:08 +0600

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,44 +1,154 @@
-import React, { useState, useEffect } from 'react';
-import { get, getDatabase, orderByKey, query, ref } from 'firebase/database';
+import React, { useState } from "react";
 
+function Chat() {
+  const [options, setOptions] = useState({
+    length: 10,
+    uppercase: false,
+    lowercase: false,
+    number: false,
+    symbols: false,
+    isError: false,
+  });
 
-const Chat = ({ quizId }) => {
-  const [leaderboard, setLeaderboard] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [generatedPassword, setGeneratedPassword] = useState("");
 
-  useEffect(() => {
-    const leaderboardRef = firebase.database().ref(`leaderboards/${quizId}`);
+  const generateRandomPassword = () => {
+    if (
+      !options?.uppercase &&
+      !options?.lowercase &&
+      !options?.number &&
+      !options?.symbols
+    ) {
+      setIsError(true);
+      return;
+    }
 
-    // Listen for real-time updates
-    leaderboardRef.on('value', (snapshot) => {
-      const data = snapshot.val();
-      if (data) {
-        // Convert data to an array or process as needed
-        const leaderboardData = Object.entries(data).map(([userId, score]) => ({
-          userId,
-          score,
-        }));
-        setLeaderboard(leaderboardData);
-      }
-    });
+    setIsError(false);
 
-    return () => {
-      // Clean up the listener when component unmounts
-      leaderboardRef.off('value');
-    };
-  }, [quizId]);
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const symbolChars = '!@#$%^&*()_+-={}[]|:;"<>,.?/~';
+
+    let passwordChars = "";
+    let password = "";
+
+    if (options.uppercase) {
+      passwordChars += uppercaseChars;
+    }
+
+    if (options.lowercase) {
+      passwordChars += lowercaseChars;
+    }
+
+    if (options.number) {
+      passwordChars += numberChars;
+    }
+
+    if (options.symbols) {
+      passwordChars += symbolChars;
+    }
+
+    const passwordLength = options.length;
+
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * passwordChars.length);
+      password += passwordChars[randomIndex];
+    }
+
+    setGeneratedPassword(password);
+  };
 
   return (
-    <div>
-      <h2>Leaderboard</h2>
-      <ul>
-        {leaderboard.map((entry) => (
-          <li key={entry.userId}>
-            {entry.userId}: {entry.score}
-          </li>
-        ))}
-      </ul>
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <p className="title">Generate Random Password</p>
+        </div>
+
+        <div className="card-body">
+          <label>Password length</label>
+          <input
+            value={options.length}
+            onChange={({ target }) => {
+              setOptions({ ...options, length: target.value });
+            }}
+            name="confirmPassword"
+            type="number"
+            placeholder="Password length"
+            min={4}
+          />
+
+          <div className="row">
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                name="languages"
+                checked={options.uppercase}
+                onChange={() => {
+                  setOptions({ ...options, uppercase: !options.uppercase });
+                }}
+              />
+              <label>Uppercase</label>
+            </div>
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                name="languages"
+                checked={options.lowercase}
+                onChange={() => {
+                  setOptions({ ...options, lowercase: !options.lowercase });
+                }}
+              />
+              <label>Lowercase</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                name="languages"
+                checked={options.number}
+                onChange={() => {
+                  setOptions({ ...options, number: !options.number });
+                }}
+              />
+              <label>Number</label>
+            </div>
+
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                name="languages"
+                checked={options.symbols}
+                onChange={() => {
+                  setOptions({ ...options, symbols: !options.symbols });
+                }}
+              />
+              <label>Symbols</label>
+            </div>
+          </div>
+
+          {isError && (
+            <span className="error">Please selete atleast one option</span>
+          )}
+
+          <button className="btn" onClick={generateRandomPassword}>
+            Generate Password
+          </button>
+        </div>
+      </div>
+
+      {generatedPassword && (
+        <div className="password">
+          <label>Generated Password:</label>
+          <p>{generatedPassword}</p>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default Chat;
