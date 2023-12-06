541781a8b3eebcea569f074a5206ad5fad5505a3
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-06 18:22:12 +0600

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,23 +1,10 @@
-import React from 'react';
-import {ChatBox } from '../components';
-
-
-
-
-function Chat() {
-  
-
+const Chat = () => {
   return (
-    <div className="Chat">
-      
-      
-        
-          <ChatBox />
-        
-    
-    </div>
+
+
+    <div className="iframe">
+    <iframe src="https://standardmindgame.vercel.app/chat.html" width="100%" height="768px"/>
+      </div>
   );
-}
-
-
+};
 export default Chat;
