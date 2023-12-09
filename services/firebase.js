c7a8514928417a01195c48349fc337b21bcd83e2
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-09 17:01:11 +0600

    Create firebase.js

---
+++ services/firebase.js
@@ -0,0 +1,29 @@
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage"
+
+// Firebase Configs
+const firebaseConfig = {
+  
+    apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
+  authDomain: "contact-database-9c47b.firebaseapp.com",
+  databaseURL: "https://admin-panel-76a17-default-rtdb.firebaseio.com",
+  projectId: "contact-database-9c47b",
+  storageBucket: "contact-database-9c47b.appspot.com",
+  messagingSenderId: "118822537955",
+  appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
+  measurementId: "G-0WY0798WFC"
+
+};
+
+// Checking if app already initialize then don't initialize again
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const auth = firebase.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+export { db, auth, googleProvider, storage };
