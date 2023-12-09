1d03c1169f117b1b5b4a330a159b6eb0438124e0
NetSMG <150570330+netsmg@users.noreply.github.com>
2023-12-09 17:37:26 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import "./assets/css/style.css";
-import "./assets/css/result.css";
-import "./assets/css/home.css";
-import "./assets/css/quiz.css";
 
-import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+createRoot(document.getElementById('root')).render(<App />);
