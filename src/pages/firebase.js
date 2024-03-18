9d8eb2ec23e0ec675d6045623568d0b306920293
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-03-19 00:50:57 +0600

    Update firebase.js

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,36 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-
-var app;
-var storage;
-
-try {
-  app = getApp();
-} catch (error) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
-  authDomain: "contact-database-9c47b.firebaseapp.com",
+const app = initializeApp({
+  apiKey: "AIzaSyB6_twOhogk9QSRWVQQ1QAekLed-cqNVgQ",
+ authDomain: "contact-database-9c47b.firebaseapp.com",
   databaseURL: "https://contact-database-9c47b-default-rtdb.firebaseio.com",
   projectId: "contact-database-9c47b",
   storageBucket: "contact-database-9c47b.appspot.com",
   messagingSenderId: "118822537955",
   appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
-  measurementId: "G-0WY0798WFC" };
-  app = initializeApp(firebaseConfig);
-}
-storage = getStorage(app);
+  measurementId: "G-0WY0798WFC"
+});
 
-// Initialize Firebase
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db, storage };
+export default app;
