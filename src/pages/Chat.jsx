74dcac7c40e4c382d8b53f017e4b5baa16a17c19
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-03-19 20:34:34 +0600

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,7 +2,9 @@ import { ChatBox } from '../components';
 
 function ChatView() { 
     return (
+    <div className="mx-auto my-24 flex w-[95%] flex-col justify-center">
        <ChatBox />
+</div>
     );
 
 }
